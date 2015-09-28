5596291516d108df9bbcab57750c086ac467a0bc
Joao Antonio Ferreira <joao.parana@gmail.com>
2015-09-28 18:38:17 -0300

    Alguns acertos

--- test/site/testecli.php
+++ test/site/testecli.php
@@ -2,6 +2,15 @@
 print_r('Testando via CLI do php. Usando o coamndo: ' . PHP_EOL .
         'docker exec web_jessie php /var/www/html/testecli.php');
 print_r(PHP_EOL);
+
+$query = "select id, name, email from CRUDClass";
+$sql = mysql_query($query)  or die(mysql_error());
+while($linha = mysql_fetch_assoc($sql)){
+  print_r($linha['name'] . PHP_EOL);;
+}
+
+return 0;
+
 include('class/mysql_crud.php');
 $db = new Database();
 print_r($db);
